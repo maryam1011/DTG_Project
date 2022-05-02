4a3409ef4e2ff8085c971560e6a9008c46aec967
Hussam Alshayeb <hos_1417@hotmail.com>
2022-05-02 03:43:23 +0300

    Support Arabic

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -29,5 +29,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+$conn->query("SET NAMES utf8");
+$conn->query("SET CHARACTER SET utf8");
 ?>
+
+  
+
 
