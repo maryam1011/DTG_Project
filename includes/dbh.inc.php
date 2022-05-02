67209191a2a4a9cf78566ae5569bf2704af9a091
Hussam Alshayeb <hos_1417@hotmail.com>
2022-05-02 07:44:14 +0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,37 +1,37 @@
-<!-- 
-
-// $servername = "eu-cdbr-west-02.cleardb.net";
-// $username = "b9b05c349b0b76";
-// $password = "b93e67c4";
-// $database = "it";
-
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $database);
-
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-//     $conn->query("SET NAMES utf8");
-//     $conn->query("SET CHARACTER SET utf8");
-
- -->
-
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "it";
 
-$conn->query("SET NAMES utf8");
-$conn->query("SET CHARACTER SET utf8");
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+    $conn->query("SET NAMES utf8");
+    $conn->query("SET CHARACTER SET utf8");
+
+
+?>
+<?php
+
+// //Get Heroku ClearDB connection information
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+// $conn->query("SET NAMES utf8");
+// $conn->query("SET CHARACTER SET utf8");
 ?>
 
   
