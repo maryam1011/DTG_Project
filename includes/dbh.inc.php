6747b5d396b7e2bb0e5fe617adfb806752def561
Hussam Alshayeb <hos_1417@hotmail.com>
2022-05-05 07:31:13 +0300

    database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,37 +1,37 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "it";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "it";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-    $conn->query("SET NAMES utf8");
-    $conn->query("SET CHARACTER SET utf8");
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+//     $conn->query("SET NAMES utf8");
+//     $conn->query("SET CHARACTER SET utf8");
 
 
 ?>
 <?php
 
-// //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-// $conn->query("SET NAMES utf8");
-// $conn->query("SET CHARACTER SET utf8");
+$conn->query("SET NAMES utf8");
+$conn->query("SET CHARACTER SET utf8");
 ?>
 
   
