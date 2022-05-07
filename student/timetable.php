008535a7135229b40f738e9f89e71b0b0b77f9df
Hussam Alshayeb <hos_1417@hotmail.com>
2022-05-07 11:02:04 +0300

    Update timetable.php

---
+++ student/timetable.php
@@ -0,0 +1,100 @@
+<?php include('header.php');?>
+
+
+<!DOCTYPE html>
+<html dir="rtl" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../style/style.css">
+    
+    <title>الجدول التدريبي</title>
+</head>
+<body class="container mx-auto">
+    
+<div class="separador"></div>
+
+<label for"id" style="text-align:center;">الرقم التدريبي
+    <input style="text-align:center;" type="text" id="id" placeholder"الرقم التدريبي للمتدربة"/>
+
+
+    <br><br><br>
+
+    <div class="overflow-x-auto w-full">
+
+    <table class="table table-compact w-full">
+    <thead>
+  <tr>
+    <th></th>
+    <th>9:00 - 10:15</th>
+    <th>11:00 - 12:45</th>
+    <th>11:00 - 12:45</th>
+    <th>11:00 - 12:45</th>
+    <th>11:00 - 12:45</th>
+  </tr>
+
+    </thead>
+    <tbody>
+
+  <tr>
+    <th>الأحد</th>
+
+    <td>رياضيات</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+
+  </tr>
+  <tr>
+    <th>الإثنين</th>
+
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+
+  </tr>
+
+  <tr>
+    <th>الثلاثاء</th>
+
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+
+  </tr>
+
+  <tr>
+    <th>الاربعاء</th>
+
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+
+  </tr>
+
+  <tr>
+    <th>الخميس</th>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+    <td>كيمياء</td>
+
+  </tr>
+</tbody>
+</table>
+
+
+
+</body>
+</html>
